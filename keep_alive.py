43cb771819c64ca87253b80ca043cd7b27b67dc0
hiuvc <153337525+hiuvc@users.noreply.github.com>
2025-09-27 11:48:42 +0700

    Update and rename keep_alive to keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,21 @@
+
+# keep_alive.py
+from flask import Flask, request
+from threading import Thread
+from datetime import datetime
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    ip = request.remote_addr
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[KEEP_ALIVE] Ping từ {ip} lúc {now}")
+    return "✅ Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
